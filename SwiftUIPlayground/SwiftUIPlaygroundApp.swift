6bb410f34ac9e740b823fd040277dbea94314c27
Raheel Ahmad <ahmadraheel@gmail.com>
2021-10-26 16:01:45 -0700

    Initial Commit

---
+++ SwiftUIPlayground/SwiftUIPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPlaygroundApp.swift
+//  SwiftUIPlayground
+//
+//  Created by Raheel Ahmad on 10/26/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
