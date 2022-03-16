1beaeebb929e1b3f50d433fc0e257f2818dca376
Raheel Ahmad <ahmadraheel@gmail.com>
2022-03-16 00:09:25 -0700

    Enum for Examples

--- SwiftUIPlayground/SwiftUIPlaygroundApp.swift
+++ SwiftUIPlayground/SwiftUIPlaygroundApp.swift
@@ -7,134 +7,64 @@
 
 import SwiftUI
 
+enum Example: String, Identifiable, CaseIterable {
+    case geometryEffectView
+    case transitionsView
+    case flowLayoutRevisited
+
+    var id: String { rawValue }
+
+    var title: String {
+        switch self {
+            case .geometryEffectView:
+                return "Geometry Effect View"
+            case .transitionsView:
+                return "Transitions View"
+            case .flowLayoutRevisited:
+                return "Flow Layout Revisited"
+        }
+    }
+}
+
+extension Example: View {
+    var body: some View {
+        switch self {
+        case .geometryEffectView:
+            GeometryEffectView()
+        case .transitionsView:
+            TransitionsView()
+        case .flowLayoutRevisited:
+
+            FlowLayoutRevisitedView(
+                items: (
+                    (1 ..< Int.random(in: 4 ..< 20)).map { _ in
+                        Item(id: UUID(), text: "Once Upon a time in a land very far away there lived a man!")
+                    }
+                        +
+                        (1 ..< Int.random(in: 5 ..< 20)).map { _ in
+                            Item(id: UUID(), text: "Lasseter")
+                        }
+                        +
+                        (1 ..< Int.random(in: 4 ..< 20)).map { _ in
+                            Item(id: UUID(), text: "Mt. Gorakh")
+                        }
+                ).shuffled()
+            )
+        }
+    }
+}
+
 @main
 struct SwiftUIPlaygroundApp: App {
     var body: some Scene {
         WindowGroup {
-            FlowLayoutRevisitedView(
-                items: [
-                    Item(
-                        id: UUID(),
-                        text: "Nowhere to go"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Lala"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Lassoto"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Once Upon a time in a land very far away there lived a man!"
-//                        text: "Once Upon a time"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Once Upon a time in a land very far away there lived a man!"
-//                        text: "Once Upon a time"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Once Upon a time in a land very far away there lived a man!"
-//                        text: "Once Upon a time"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Once Upon a time in a land very far away there lived a man!"
-//                        text: "Once Upon a time"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Monse1q"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Terminus"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Massive"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Lassoto"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Lassoto"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Lassoto"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Lassoto"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Lassoto"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Lassoto"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Lassoto"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Lassoto"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Lassoto"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Lassoto"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Lassoto"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Lassoto"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Lassoto"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Lassoto"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Lassoto"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Lassoto"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Lassoto"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Pakelate"
-                    ),
-                    Item(
-                        id: UUID(),
-                        text: "Starrett"
-                    ),
-                ]
-            )
+            NavigationView {
+                List(Example.allCases) { example in
+                    NavigationLink(example.title) {
+                        example
+                    }
+                }
+            }
         }
     }
 }
